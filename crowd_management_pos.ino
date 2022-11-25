4018d80e8faa348ad2ad44bae781389c161372cd
IshitaBharadwaj <ishitabharadwaj04@gmail.com>
2022-11-25 22:03:59 +0530

    corrected basic code

--- crowd_management_pos.ino
+++ crowd_management_pos.ino
@@ -4,8 +4,8 @@ int in_a = 15;
 int inpr_a = 16;
 int out_a = 14;
 int outpr_a = 17;
-int in_b =1;
-int inpr_b = 0;
+int in_b = 10;
+int inpr_b = 13;
 int out_b = 19;
 int outpr_b = 18;
 int in_c = 9;
@@ -13,8 +13,8 @@ int inpr_c = 8;
 int out_c = 7;
 int outpr_c = 6;
 int ppl_a = 0;
-int ppl_b=0;
-int ppl_c=0;
+int ppl_b = 0;
+int ppl_c = 0;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 bool pi_a = 0;
 bool po_a = 0;
@@ -22,38 +22,40 @@ bool pi_b = 0;
 bool po_b = 0;
 bool pi_c = 0;
 bool po_c = 0;
-void setup() {
-  pinMode(15, INPUT);
-  pinMode(14, INPUT);
-  pinMode(1, INPUT);
-  pinMode(19, INPUT);
-  pinMode(9, INPUT);
-  pinMode(7, INPUT);
-  pinMode(16, OUTPUT);
-  pinMode(17, OUTPUT);
-  pinMode(0, OUTPUT);
-  pinMode(18, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(6, OUTPUT);
-  lcd.begin(16, 2);
-  Serial.begin(9600);
+void setup()
+{
+    pinMode(15, INPUT);
+    pinMode(14, INPUT);
+    pinMode(10, INPUT);
+    pinMode(19, INPUT);
+    pinMode(9, INPUT);
+    pinMode(7, INPUT);
+    pinMode(16, OUTPUT);
+    pinMode(17, OUTPUT);
+    pinMode(13, OUTPUT);
+    pinMode(18, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(6, OUTPUT);
+    lcd.begin(16, 2);
+    Serial.begin(9600);
 }
 
-void loop() {
-  lcd.clear();
-  digitalWrite(outpr_a, HIGH);
-  digitalWrite(inpr_a, HIGH);
+void loop()
+{
+    lcd.clear();
+    digitalWrite(outpr_a, HIGH);
+    digitalWrite(inpr_a, HIGH);
     digitalWrite(outpr_b, HIGH);
     digitalWrite(inpr_b, HIGH);
     digitalWrite(outpr_c, HIGH);
     digitalWrite(inpr_c, HIGH);
-  pi_a = digitalRead(in_a);
-  po_a = digitalRead(out_a);
-  pi_b = digitalRead(in_b);
-  po_b = digitalRead(out_b);
-  pi_c = digitalRead(in_c);
-  po_c = digitalRead(out_c);
-  if ((ppl_a != 10 || ppl_b != 10))
+    pi_a = digitalRead(in_a);
+    po_a = digitalRead(out_a);
+    pi_b = digitalRead(in_b);
+    po_b = digitalRead(out_b);
+    pi_c = digitalRead(in_c);
+    po_c = digitalRead(out_c);
+    if ((ppl_a != 10 || ppl_b != 10))
     {
 
         if ((po_a == 1 || po_b == 1))
@@ -138,7 +140,7 @@ void loop() {
         }
         else if (pi_b == 1 && ppl_b == 0)
         {
-            Serial.println("No customers in B");
+            Serial.println("No customers in B test 1");
             delay(1000);
         }
         if (pi_c == 1 && ppl_c > 0)
@@ -224,7 +226,7 @@ void loop() {
         }
         else if (pi_b == 1 && ppl_b == 0)
         {
-            Serial.println("No customers in B");
+            Serial.println("No customers in B test 2");
             delay(1000);
         }
         if (pi_c == 1 && ppl_c > 0)
